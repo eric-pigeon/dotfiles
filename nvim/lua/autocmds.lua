f77a0cd5ee35e4ebb56f6a7032175ba5fd148534
Eric Pigeon <eric.r.pigeon@gmail.com>
2023-12-07 09:54:13 -0500

    Enable large file autocmd

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,29 +1,37 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 local utils = require "utils"
+local event = utils.event
 
---autocmd("BufReadPre", {
---  desc = "Disable certain functionality on very large files",
---  group = augroup("large_buf", { clear = true }),
---  callback = function(args)
---    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(args.buf))
---    vim.b[args.buf].large_buf = (ok and stats and stats.size > vim.g.max_file.size)
---      or vim.api.nvim_buf_line_count(args.buf) > vim.g.max_file.lines
---  end,
---})
+autocmd("BufReadPre", {
+  desc = "Disable certain functionality on very large files",
+  group = augroup("large_buf", { clear = true }),
+  callback = function(args)
+    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(args.buf))
+    vim.b[args.buf].large_buf = (ok and stats and stats.size > vim.g.max_file.size)
+      or vim.api.nvim_buf_line_count(args.buf) > vim.g.max_file.lines
+  end,
+})
 
 local bufferline_group = augroup("bufferline", { clear = true })
 autocmd({ "BufAdd", "BufEnter", "TabNewEntered" }, {
   desc = "Update buffers when adding new buffers",
   group = bufferline_group,
   callback = function(args)
+    local buf_utils = require "utils.buffer"
     if not vim.t.bufs then vim.t.bufs = {} end
+    if not buf_utils.is_valid(args.buf) then return end
+    if args.buf ~= buf_utils.current_buf then
+      buf_utils.last_buf = buf_utils.is_valid(buf_utils.current_buf) and buf_utils.current_buf or nil
+      buf_utils.current_buf = args.buf
+    end
     local bufs = vim.t.bufs
     if not vim.tbl_contains(bufs, args.buf) then
       table.insert(bufs, args.buf)
       vim.t.bufs = bufs
     end
     vim.t.bufs = vim.tbl_filter(require("utils.buffer").is_valid, vim.t.bufs)
+    event "BufsUpdated"
   end,
 })
 autocmd({ "BufDelete", "TermClose" }, {
@@ -45,7 +53,7 @@ autocmd({ "BufDelete", "TermClose" }, {
       end
     end
     vim.t.bufs = vim.tbl_filter(require("utils.buffer").is_valid, vim.t.bufs)
-    if removed then utils.event "BufsUpdated" end
+    if removed then event "BufsUpdated" end
     vim.cmd.redrawtabline()
   end,
 })
@@ -87,7 +95,7 @@ autocmd({ "BufReadPost", "BufNewFile" }, {
   callback = function(args)
     local current_file = vim.fn.resolve(vim.fn.expand "%")
     if not (current_file == "" or vim.api.nvim_get_option_value("buftype", { buf = args.buf }) == "nofile") then
-      utils.event "File"
+      event "File"
       -- if
       --   require("utils.git").file_worktree()
       --   or utils.cmd({ "git", "-C", vim.fn.fnamemodify(current_file, ":p:h"), "rev-parse" }, false)
