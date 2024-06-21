239cfe3ae52152471d6df684c2306206c5443961
Eric Pigeon <eric.r.pigeon@gmail.com>
2024-06-21 16:15:14 -0400

    Fix guess-indent

--- nvim/lua/plugins/configs/guess-indent.lua
+++ nvim/lua/plugins/configs/guess-indent.lua
@@ -1,4 +1,1 @@
-return function(_, opts)
-  require("guess-indent").setup(opts)
-  vim.cmd.lua { args = { "require('guess-indent').set_from_buffer('auto_cmd')" }, mods = { silent = true } }
-end
+return function(_, opts) require("guess-indent").setup(opts) end
